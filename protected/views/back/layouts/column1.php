022be7e6dfe4a947fff5e7eee41f8c9f291257e7
tuandat92 <n.tuandat92@gmail.com>
2015-05-19 15:11:27 +0700

    Revert "First Commit"
    This reverts commit dd3c79500f3c3d7616bdb22aafc4bfd2f08ceb0f.

--- protected/views/back/layouts/column1.php
+++
@@ -1,9 +0,0 @@
-<?php $this->beginContent('//layouts/main'); ?>
-<div id="content">
-	<div class="row">
-		<div class="span12">
-	<?php echo $content; ?>
-		</div>
-	</div>
-</div><!-- content -->
-<?php $this->endContent(); ?>
--- protected/views/back/layouts/column2.php
+++
@@ -1,8 +0,0 @@
-<?php $this->beginContent('//layouts/main');?>
-<div class="row">
-<div class="span8">
-		<?php echo $content; ?>
-</div>
-
-</div>
-<?php $this->endContent(); ?>
